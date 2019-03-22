65ba24c0269ef66337f3949ca214182293c1fdc7
Jose Elera <jelera@gmail.com>
2019-03-22 23:11:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(sentence)
   # arr = sentence.split(" ")
-  # 
+  #
   # output = []
-  # 
+  #
   # arr.each do |str|
   #   output << str.reverse
   # end
-  # 
+  #
   # output.join(" ")
 
   arr = sentence.split(" ")
