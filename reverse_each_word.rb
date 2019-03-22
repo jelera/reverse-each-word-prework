77fa5fe7a000b7c564674e2fe89a2697ab3ae024
Jose Elera <jelera@gmail.com>
2019-03-22 23:11:29 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word(sentence)
+  # arr = sentence.split(" ")
+  #
+  # output = []
+  #
+  # arr.each do |str|
+  #   output << str.reverse
+  # end
+  #
+  # output.join(" ")
+
+  arr = sentence.split(" ")
+
+
+  output = arr.collect do | word |
+    word.reverse
+  end
+
+  output.join(" ")
+
+
+end
